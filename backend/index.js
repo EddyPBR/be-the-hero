bf2139c94f8ff31e6b7d77321aa11bd5549e1e4d
Edvaldo Junior <edvaldojunior1310@gmail.com>
2020-03-23 10:49:35 -0300

    adicionando o express no backend e o react no frotend

--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    return res.json({
+        project: 'Semana Oministack 11',
+        aluno: 'Edvaldo Junior',
+    });
+});
+
 app.listen(3333);
 

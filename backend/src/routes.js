2328d89ec34178ed7b9d7ed12a92d9935792ef37
Edvaldo Junior <edvaldojunior1310@gmail.com>
2020-03-24 14:26:35 -0300

    adicionando arquivo de anotacoes do projeto, e restruturando arquivos e pastas do projeto

---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const routes = express.Router();
+
+routes.post('/users', (request, response) => {
+    const body = request.body;
+    console.log(body);
+
+    return response.json({
+        evento: 'Semana Oministack 11',
+        aluno: 'Edvaldo Junior',
+    });
+});
+
+module.exports = routes;

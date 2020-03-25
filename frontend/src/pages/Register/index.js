6f1e57bbba5f3ba8491922d59564195bab27b223
Edvaldo Junior <edvaldojunior1310@gmail.com>
2020-03-25 19:28:44 -0300

    adicionando pagina de registro

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
+import { FiArrowLeft } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
 export default function Register() {
     return ( 
-        <h1>Register</h1> 
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero" />
+
+                    <h1>Cadastro</h1>
+                    <p>
+                        Faça seu cadastro, entre na plataforma e ajude pessoas 
+                        a encontrarem os casos da sua ONG.
+                    </p>
+
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link>
+
+                </section>
+                <form>
+                    <input placeholder="Nome da ONG" />
+                    <input type="email" placeholder="E-Mail" />
+                    <input placeholder="Whatsapp" />
+
+                    <div className="input-group">
+                        <input placeholder="Cidade" />
+                        <input placeholder="UF" style={{ width: 80 }} />
+                    </div>
+
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
     );
 }

0fa3a58cb7047184eeb8663bbf5129597ef576b4
Edvaldo Junior <edvaldojunior1310@gmail.com>
2020-03-25 10:28:13 -0300

    propriedades em react um exemplo entre app.js e header.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import Header from './Header';
 
-function App() {
+export default function App() {
   return (
-    <h1>Hello World</h1>
+    <Header title="Hello Edvaldo" />
   );
 }
-
-export default App;

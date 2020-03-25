96160b78c36e7a546b71cbe64f1162f0c29466e0
Edvaldo Junior <edvaldojunior1310@gmail.com>
2020-03-25 17:14:36 -0300

    adicionando o react-icons, pagina logon e estilos da mesma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+
+import './global.css';
+
+import Logon from './pages/Logon';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
-  );
+    <Logon />
+  ); 
 }
--- frontend/src/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Header({ children }) { 
-    return(
-        <h1>{children}</h1>
-    );
-}

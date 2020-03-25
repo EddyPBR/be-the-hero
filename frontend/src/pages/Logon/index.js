3302d52240a43205b88b3da48a47461961f332be
Edvaldo Junior <edvaldojunior1310@gmail.com>
2020-03-25 18:03:37 -0300

    adicionado a tag link na pagina logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './styles.css';
 
 import { FiLogIn } from 'react-icons/fi';
@@ -17,10 +19,10 @@ export default function Logon() {
                     <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register">
+                    <Link to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // importando paginas
 import Logon from './pages/Logon';
+import Register from './pages/Register';
 
 export default function Routes() {
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Logon} />
+                <Route path="/" exact component={Logon} />
+                <Route path="/register" component={Register} />
             </Switch>
         </BrowserRouter>
     );

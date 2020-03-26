8ce404ac272a848e5207a3a089fc66fa06370da6
Edvaldo Junior <edvaldojunior1310@gmail.com>
2020-03-25 22:27:42 -0300

    habilitando o uso da api do backend e habilitando o cadastro de ongs na mesma

--- backend/src/controllers/OngContrller.js
+++ backend/src/controllers/OngContrller.js
@@ -1,5 +1,5 @@
-const connection = require('../database/connection');
 const crypto = require("crypto");
+const connection = require('../database/connection');
 
 module.exports = {
     // listar ong

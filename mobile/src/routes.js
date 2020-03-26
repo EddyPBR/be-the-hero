377ced59726888f72dc348daced88befb7f39274
Edvaldo Junior <edvaldojunior1310@gmail.com>
2020-03-26 18:53:57 -0300

    adicionando as rotas e pagina de incidents ja estilizada

---
+++ mobile/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+// pages
+import Incidents from './pages/Incidents';
+import Detail from './pages/Detail';
+
+export default function Routes() {
+    return(
+        <NavigationContainer>
+
+            <AppStack.Navigator screenOptions={{ headerShow: false }}>
+                <AppStack.Screen name="Incidents" component={Incidents} />
+                <AppStack.Screen name="Detail" component={Detail} />
+            </AppStack.Navigator>
+
+        </NavigationContainer>
+    );
+}
